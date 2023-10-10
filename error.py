e97abd97b2ebdb39abb263e5aa2980459e6edc45
DuyenNguyen <88462976+walo0101@users.noreply.github.com>
2023-10-10 13:43:59 +0200

    Update error.py

--- error.py
+++ error.py
@@ -44,3 +44,4 @@ def absolute_percentage_error_std(y_true, y_pred):
     return np.std(_absolute_percentage_error(y_true, y_pred))
 
 mse = tf.keras.metrics.mean_squared_error(y_true, y_pred)
+rmse = np.sqrt(mse)
