59b9cdd47ea7bed2930a01f566a8ec557dd7830d
DuyenNguyen <88462976+walo0101@users.noreply.github.com>
2023-10-25 16:39:50 +0200

    Update writeFile.py

--- writeFile.py
+++ writeFile.py
@@ -1,3 +1,11 @@
 import h5py
 with h5py.File('node.hdf5', 'w') as f:
     dataset = f.create_dataset("node", (node.shape[0],node.shape[1]), data=node)
+
+
+#%% read h5df file
+import h5py
+with h5py.File('node.hdf5', 'r') as f1:
+    print(list(f1.keys()))  # print list of root level objects
+    node = f1['node']  
+    node = node[:]
