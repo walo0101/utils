77ed6bf74c3a22ede244a590c121be4f0904470d
DuyenNguyen <88462976+walo0101@users.noreply.github.com>
2023-12-15 13:51:42 +0100

    Update writeFile.py

--- writeFile.py
+++ writeFile.py
@@ -1,3 +1,10 @@
+# write csv file
+import csv
+with open('E:/Back up/saveCSV/Utest_pca11_1hidl_LSTM_200_2000ep.csv', 'w+', encoding='UTF8', newline='') as f:
+    writer = csv.writer(f, delimiter=',')
+    writer.writerows(U_pred)
+
+# write hdf5 file
 import h5py
 with h5py.File('node.hdf5', 'w') as f:
     dataset = f.create_dataset("node", (node.shape[0],node.shape[1]), data=node)
